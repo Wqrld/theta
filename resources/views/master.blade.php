a1b06d598fad67fa1dd66ad6174a40c58572b72d
Ferox <wereld03@gmail.com>
2018-11-05 14:56:46 +0100

    stub some data

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,7 +7,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Starter</title>
+  <title>Theta</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
@@ -64,9 +64,9 @@ desired effect
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>T</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b>LTE</span>
+      <span class="logo-lg"><b>Theta</b></span>
     </a>
 
     <!-- Header Navbar -->
@@ -98,11 +98,11 @@ desired effect
                       </div>
                       <!-- Message title and timestamp -->
                       <h4>
-                        Support Team
+                        Janssen
                         <small><i class="fa fa-clock-o"></i> 5 mins</small>
                       </h4>
                       <!-- The message -->
-                      <p>Why not buy a new awesome theme?</p>
+                      <p>Can you come and finish your homework?</p>
                     </a>
                   </li>
                   <!-- end message -->
@@ -119,16 +119,16 @@ desired effect
             <!-- Menu toggle button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-bell-o"></i>
-              <span class="label label-warning">10</span>
+              <span class="label label-warning">1</span>
             </a>
             <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
+              <li class="header">You have 1 notification(s)</li>
               <li>
                 <!-- Inner Menu: contains the notifications -->
                 <ul class="menu">
                   <li><!-- start notification -->
                     <a href="#">
-                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                      <i class="fa fa-users text-aqua"></i> lesson 3, french has been cancelled
                     </a>
                   </li>
                   <!-- end notification -->
@@ -138,8 +138,8 @@ desired effect
             </ul>
           </li>
           <!-- Tasks Menu -->
-          <li class="dropdown tasks-menu">
-            <!-- Menu Toggle Button -->
+        <!--  <li class="dropdown tasks-menu">
+             Menu Toggle Button
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-flag-o"></i>
               <span class="label label-danger">9</span>
@@ -147,18 +147,18 @@ desired effect
             <ul class="dropdown-menu">
               <li class="header">You have 9 tasks</li>
               <li>
-                <!-- Inner menu: contains the tasks -->
+                <!-- Inner menu: contains the tasks
                 <ul class="menu">
-                  <li><!-- Task item -->
+                  <li><!-- Task item
                     <a href="#">
-                      <!-- Task title and progress text -->
+                      <!-- Task title and progress text
                       <h3>
                         Design some buttons
                         <small class="pull-right">20%</small>
                       </h3>
-                      <!-- The progress bar -->
+                      <!-- The progress bar
                       <div class="progress xs">
-                        <!-- Change the css width attribute to simulate progress -->
+                        <!-- Change the css width attribute to simulate progress
                         <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
                              aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
                           <span class="sr-only">20% Complete</span>
@@ -166,14 +166,14 @@ desired effect
                       </div>
                     </a>
                   </li>
-                  <!-- end task item -->
+                  <!-- end task item
                 </ul>
               </li>
               <li class="footer">
                 <a href="#">View all tasks</a>
               </li>
             </ul>
-          </li>
+          </li>-->
           <!-- User Account Menu -->
           <li class="dropdown user user-menu">
             <!-- Menu Toggle Button -->
@@ -181,7 +181,7 @@ desired effect
               <!-- The user image in the navbar-->
               <img src="img/user2-160x160.jpg" class="user-image" alt="User Image">
               <!-- hidden-xs hides the username on small devices so only the image appears. -->
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs">Luc H</span>
             </a>
             <ul class="dropdown-menu">
               <!-- The user image in the menu -->
@@ -189,11 +189,11 @@ desired effect
                 <img src="img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                  Luc H
+                  <small>4v1</small>
                 </p>
               </li>
-              <!-- Menu Body -->
+              <!-- Menu Body
               <li class="user-body">
                 <div class="row">
                   <div class="col-xs-4 text-center">
@@ -206,8 +206,8 @@ desired effect
                     <a href="#">Friends</a>
                   </div>
                 </div>
-                <!-- /.row -->
-              </li>
+                <!-- /.row
+              </li>-->
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
@@ -239,9 +239,9 @@ desired effect
           <img src="img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p>Luc H</p>
           <!-- Status -->
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+          <a href="#"><i class="fa fa-circle text-success"></i> At school</a>
         </div>
       </div>
 
@@ -259,11 +259,11 @@ desired effect
 
       <!-- Sidebar Menu -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">HEADER</li>
+        <li class="header">STUDENT</li>
         <!-- Optionally, you can add icons to the links -->
-        <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-        <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-        <li class="treeview">
+        <li class="active"><a href="#"><i class="fa fa-link"></i> <span>today</span></a></li>
+        <li><a href="#"><i class="fa fa-link"></i> <span>schedule</span></a></li>
+        <!--<li class="treeview">
           <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
             <span class="pull-right-container">
                 <i class="fa fa-angle-left pull-right"></i>
@@ -273,7 +273,7 @@ desired effect
             <li><a href="#">Link in level 2</a></li>
             <li><a href="#">Link in level 2</a></li>
           </ul>
-        </li>
+        </li>-->
       </ul>
       <!-- /.sidebar-menu -->
     </section>
@@ -283,25 +283,7 @@ desired effect
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Page Header
-        <small>Optional description</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
-      </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content container-fluid">
-
-      <!--------------------------
-        | Your Page Content Here |
-        -------------------------->
-@yield('content')
-    </section>
+    @yield('content')
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
@@ -313,7 +295,7 @@ desired effect
       Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2018 <a href="#">Your school</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
@@ -334,7 +316,7 @@ desired effect
               <i class="menu-icon fa fa-birthday-cake bg-red"></i>
 
               <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
+                <h4 class="control-sidebar-subheading">Next testweek</h4>
 
                 <p>Will be 23 on April 24th</p>
               </div>
@@ -348,9 +330,9 @@ desired effect
           <li>
             <a href="javascript:;">
               <h4 class="control-sidebar-subheading">
-                Custom Template Design
+                time until test week
                 <span class="pull-right-container">
-                    <span class="label label-danger pull-right">70%</span>
+                    <span class="label label-danger pull-right">2 days</span>
                   </span>
               </h4>
 
